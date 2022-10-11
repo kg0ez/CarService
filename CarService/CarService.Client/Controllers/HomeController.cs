68a33294fc7d70cc1902de182c58d071bf57a2e6
kg0ez <82456927+kg0ez@users.noreply.github.com>
2022-10-11 06:51:09 +0300

    empty model

---
+++ CarService/CarService.Client/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CarService.Client.Models;
+
+namespace CarService.Client.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
---
+++ CarService/CarService.Client/Models/EmptyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CarService.Client.Models
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
+
